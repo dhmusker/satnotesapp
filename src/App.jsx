3e32162d92b9a3b424156d3f17df45a00d21649c
Dermot Musker <dhmusker@gmail.com>
2024-08-03 13:36:56 +1000

    Adding Incident Mgt page

--- src/App.jsx
+++ src/App.jsx
@@ -28,6 +28,11 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <div>
+        <a href="IncidentMgt.html" target="_blank">
+          Incident Management
+        </a>
+      </div>
     </>
   )
 }
